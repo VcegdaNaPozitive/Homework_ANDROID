5f05caa9a72fd53d6171c2c6f9cb401bcfbfc0f0
rsnix <rsnix@bk.ru>
2022-11-29 23:51:28 +0300

    Homework_13-14

--- app/src/main/java/com/example/it_school_samsung/ActivityTwo.java
+++ app/src/main/java/com/example/it_school_samsung/ActivityTwo.java
@@ -2,15 +2,21 @@ package com.example.it_school_samsung;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.it_school_samsung.databinding.ActivityTwoBinding;
 
-public class ActivityTwo extends AppCompatActivity {
+public class ActivityTwo extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityTwoBinding binding;
+    private ImageView photo;
+    private static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,29 @@ public class ActivityTwo extends AppCompatActivity {
         Intent intent = getIntent();
         String login = intent.getStringExtra("login");
         binding.hello.setText(login);
+        binding.camera.setOnClickListener(this);
+        photo = binding.photo;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try{
+            startActivityForResult(takePhotoIntent, REQUEST_TAKE_PHOTO);
+        }catch (ActivityNotFoundException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap thumbnailBitmap = (Bitmap) extras.get("data");
+            photo.setImageBitmap(thumbnailBitmap);
+        }
     }
 }
+
--- app/src/main/java/com/example/it_school_samsung/MainActivity.java
+++ app/src/main/java/com/example/it_school_samsung/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,18 +26,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding.button.setOnClickListener(this);
         login = binding.editLogin;
         password = binding.editPassword;
-
     }
 
 
     @Override
     public void onClick(View view) {
+        Log.v("LET'S GO", "START");
         if (login.getText().length() == 0 || password.getText().length() == 0) {
             Toast.makeText(this, "Заполните пустые поля", Toast.LENGTH_SHORT).show();
+            Log.e("ERR", "Надо поля заполнить");
         } else if (password.getText().length() < 8) {
             Toast.makeText(this, "Пароль должен быть не менее 8 символов", Toast.LENGTH_SHORT).show();
+            Log.e("ERR", "Пароль нормальный нужен");
         } else if (login.getText().toString().equals("rickroll@gmail.com") && !password.getText().toString().equals("RickAstley")) {
             Toast.makeText(this, "Неверный пароль", Toast.LENGTH_SHORT).show();
+            Log.e("ERR", "Фигня, пробовать другое надо");
         } else {
 
             switch (view.getId()) {
@@ -44,9 +48,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Intent intent = new Intent(MainActivity.this, ActivityTwo.class);
                     intent.putExtra("login", binding.editLogin.getText().toString());
                     startActivity(intent);
+                    Log.i("YES", "email: " + binding.editLogin.getText().toString());
                     break;
             }
         }
     }
-
 }
