2ebc6f81f7b38d5087a4b7f2d3e78c3e2647c5ff
rsnix <rsnix@bk.ru>
2022-11-29 20:18:11 +0300

    Homework_13-14

--- app/src/main/java/com/example/it_school_samsung/MainActivity.java
+++ app/src/main/java/com/example/it_school_samsung/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.it_school_samsung.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityMainBinding binding;
+    private EditText login;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +22,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.btnActTwo.setOnClickListener(this);
+        binding.button.setOnClickListener(this);
+        login = binding.editLogin;
+        password = binding.editPassword;
+
     }
 
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btnActTwo:
-                Intent intent = new Intent(MainActivity.this, ActivityTwo.class);
-                startActivity(intent);
-                break;
+        if  (login.getText().length() == 0 || password.getText().length() == 0) {
+            Toast.makeText(this, "Заполните пустые поля", Toast.LENGTH_SHORT).show();
+        }
+        else if (password.getText().length() < 8) {
+            Toast.makeText(this, "Пароль должен быть не менее 8 символов", Toast.LENGTH_SHORT).show();
+        } else {
+            switch (view.getId()) {
+                case R.id.button:
+                    Intent intent = new Intent(MainActivity.this, ActivityTwo.class);
+                    startActivity(intent);
+                    break;
+            }
         }
     }
 
